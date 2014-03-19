aee03fed6b5627bc545a6a199462981ef5d3457e
saggammahesh <saggammahesh@gmail.com>
2014-03-19 21:39:57 +0530

    a commit from TOMODO - JS File

---
+++ JS.js
@@ -0,0 +1,30 @@
+/**
+ * TOMODO
+ * 
+ * This file holds the user script written by YOU.
+ * It only appears in Preview mode and its purpose is to allow you to debug your script.
+ * Put your breakpoints below.
+ * 
+ * Thanks,
+ * the TOMODO Team.  
+ */
+
+function TOMDO_runUserScript(){
+	//----------------------------------- Your Code Here ------------------------------------	
+	/**********************************************
+Add your Javascript code below.
+
+To debug your code use Preview mode and
+debug tomodouserscript.js using
+Chrome Developer Tools, Firebug or another Javascript debugger.
+**********************************************/
+
+bi$('html > body > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-of-type(1) > td > span.white-txt > table > tbody > tr:nth-of-type(2) > td > table > tbody > tr > td > table > tbody > tr > td:nth-of-type(7) > table > tbody > tr > td > ul.dropdown > li > ul > li:nth-of-type(4) > a').html( 'test' );	
+	//---------------------------------------------------------------------------------------	
+}
+
+function TOMODO_runWYSIWYGScript(){
+	//----------------------------------- WYSIWYG Auto generated code -----------------------
+	
+	//---------------------------------------------------------------------------------------
+}
